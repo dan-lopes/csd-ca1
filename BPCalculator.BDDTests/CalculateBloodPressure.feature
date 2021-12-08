4ade6b54b023bf0e894d06263f387430de27c8fa
WH5T <danilolopes@eurofins.ie>
2021-12-08 15:45:37 -0300

    Added BDD test - Specflow

---
+++ BPCalculator.BDDTests/CalculateBloodPressure.feature
@@ -0,0 +1,7 @@
+﻿
+Feature: CalculateBloodPressure
+
+Scenario: BloodPressureIdeal
+    Given a patient with blood pressure readings of 90 mm Hg systolic and 60 mm Hg diastolic
+    When I request the blood pressure calculation
+    Then the blood pressure is considered Low
---
+++ BPCalculator.BDDTests/CalculateBloodPressureFeature.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace BPCalculator.BDDTests
+{
+    [Binding]
+    public class CalculateBloodPressureFeature
+    {
+        private int _systolic;
+        private int _diastolic;
+        private BPCategory _bpCategory;
+
+        [Given("a patient with blood pressure readings of (.*) mm Hg systolic and (.*) mm Hg diastolic")]
+        public void GivenAPatientWithBloodPressureReadingsOfSystolicMmHgSystolicAndDiastolicMmHgDiastolic(int systolic, int diastolic)
+        {
+            _systolic = systolic;
+            _diastolic = diastolic;
+        }
+
+        [When("I request the blood pressure calculation")]
+        public void WhenIRequestTheBloodPressureCalculation()
+        {
+            var bpCalculator = new BloodPressure
+            {
+                Systolic = _systolic,
+                Diastolic = _diastolic
+            };
+
+            _bpCategory = bpCalculator.Category;
+        }
+
+        [Then("the blood pressure is considered (.*)")]
+        public void ThenTheBloodPressureIsConsideredCategory(string category)
+        {
+            Enum.TryParse(category, out BPCategory bpCategory);
+
+            Assert.Equal(bpCategory, _bpCategory);
+        }
+    }
+}
