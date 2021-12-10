a551ee1483bb187f83d80f57067bf7a1aba17b54
WH5T <danilolopes@eurofins.ie>
2021-12-10 11:20:17 -0300

    Improved layout

--- BPCalculator/wwwroot/js/site.js
+++ BPCalculator/wwwroot/js/site.js
@@ -2,3 +2,28 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+function setAlert() {
+    let actionMessage = $('#alert-message');
+    actionMessage.addClass('visually-hidden');
+
+    actionMessage.removeClass('alert-success');
+    actionMessage.removeClass('alert-warning');
+    actionMessage.removeClass('alert-danger');
+    actionMessage.removeClass('alert-info');
+
+    if(actionMessage.html().lastIndexOf("Low") >= 0){
+        actionMessage.addClass('alert-info');
+    }
+    else if(actionMessage.html().lastIndexOf("Ideal") >= 0){
+        actionMessage.addClass('alert-success');
+    }
+    else if(actionMessage.html().lastIndexOf("Pre-High") >= 0){
+        actionMessage.addClass('alert-warning');
+    }
+    else if(actionMessage.html().lastIndexOf("High") >= 0){
+        actionMessage.addClass('alert-danger');
+    }
+
+    actionMessage.removeClass('visually-hidden');
+}
