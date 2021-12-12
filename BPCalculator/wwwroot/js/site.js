b73ab04a164b385de274dbb742361308e2cb83d5
WH5T <danilolopes@eurofins.ie>
2021-12-12 16:41:42 -0300

    Added new feature - Gauge Charts

--- BPCalculator/wwwroot/js/site.js
+++ BPCalculator/wwwroot/js/site.js
@@ -1,4 +1,41 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function drawChart() {
 
-// Write your Javascript code.
+    let systolic = parseInt($("#BP_Systolic").val());
+    let diastolic = parseInt($('#BP_Diastolic').val());
+
+    let data_systolic = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Systolic', systolic ]
+    ]);
+
+    let data_diastolic = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Diastolic', diastolic ]
+    ]);
+
+    let options_systolic = {
+        width: 300, height: 200,
+        min: 70, max: 190,
+        greenFrom: 90, greenTo: 120,
+        yellowFrom: 120, yellowTo: 140,
+        redFrom: 140, redTo: 190,
+        majorTicks: 0,
+        minorTicks: 0
+    };
+
+    let options_diastolic = {
+        width: 300, height: 200,
+        min: 40, max: 100,
+        greenFrom: 60, greenTo: 80,
+        yellowFrom: 80, yellowTo: 90,
+        redFrom: 90, redTo: 100,
+        majorTicks: 0,
+        minorTicks: 0
+    };
+
+    let chart_systolic = new google.visualization.Gauge(document.getElementById('chart_div_systolic'));
+    let chart_diastolic = new google.visualization.Gauge(document.getElementById('chart_div_diastolic'));
+
+    chart_systolic.draw(data_systolic, options_systolic);
+    chart_diastolic.draw(data_diastolic, options_diastolic);
+}
